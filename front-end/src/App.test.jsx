5c46827eedde90b6d664e3f8a3698f369a9f5fcf
Vani Kamra <vani@mac.lan>
2026-01-28 22:43:20 -0500

    Fix App test by adding Router wrapper

--- front-end/src/App.test.jsx
+++ front-end/src/App.test.jsx
@@ -1,22 +1,28 @@
 import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from "react-router-dom";
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('something truthy and falsy', () => {
-    it('true to be true', () => {
-        expect(true).toBe(true);
-    });
+  it('true to be true', () => {
+    expect(true).toBe(true);
+  });
 
-    it('false to be false', () => {
-        expect(false).toBe(false);
-    });
+  it('false to be false', () => {
+    expect(false).toBe(false);
+  });
 });
 
-
 describe('App', () => {
-    it('renders headline', () => {
-        render(<App title="React" />);
-        screen.debug();
-        // check if App components renders headline
-    });
+  it('renders headline', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>
+    );
+
+    expect(
+      screen.getByText(/Welcome to the OTC Medical Supplies App!/i)
+    ).toBeInTheDocument();
+  });
 });
